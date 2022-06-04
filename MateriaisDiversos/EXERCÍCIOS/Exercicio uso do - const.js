96f8128656bd2d9948d26a03ad2650a2d33929bb
HWUlrich <hansfisica@gmail.com>
2022-06-04 17:57:48 -0300

    Exercicios Menu Hamburger

---
+++ MateriaisDiversos/EXERCÍCIOS/Exercicio uso do - const.js
@@ -0,0 +1,8 @@
+//Create a array
+const car ["Saab", "Volvo", "BMW"];
+//Change a element
+cars[0] = "Toyota";
+//Add a element
+cars.push("Audi");
+//Display the Array
+document.getElementById("demo").innerHTML = cars;
---
+++ MateriaisDiversos/EXERCÍCIOS/Exercicio uso do - const1.js
@@ -0,0 +1,8 @@
+//Create an Object
+const car = {Type:"Fiat", model:"500", color:"White"};
+//Change a color
+car.color = "red";
+//Change a property
+car.owner = "Johnson";
+//Dispaly the property
+document.getElementById("demo").innerHTML = "Car owner is:" + " " + car.owner;
